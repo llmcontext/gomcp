0552952de38d6920a11b79dd8f7f54240cdb938f
Pierre Carion <pcarion@gmail.com>
2024-12-13 06:58:42 -0800

    change cli parameter for proxy

--- cmd/gomcp-proxy/main.go
+++ cmd/gomcp-proxy/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 
 	"github.com/llmcontext/gomcp/defaults"
@@ -12,7 +13,7 @@ import (
 )
 
 var (
-	port    int
+	address string
 	rootCmd = &cobra.Command{
 		Use:   "gomcp-proxy",
 		Short: "A proxy server for MCP connections",
@@ -26,10 +27,16 @@ var (
 			programName := args[0]
 			args = args[1:]
 
+			// check if address is valid
+			if _, err := net.ResolveTCPAddr("tcp", address); err != nil {
+				fmt.Printf("Invalid address for MCP Proxy: %s, err: %s\n", address, err)
+				os.Exit(1)
+			}
+
 			// Print an informational message using PTerm's Info printer.
 			// This message will stay in place while the area updates.
 			pterm.Info.Println("MCP Proxy is starting")
-			pterm.Info.Println(fmt.Sprintf("- ws port is: %d\n", port))
+			pterm.Info.Println(fmt.Sprintf("- ws address is: %s\n", address))
 			pterm.Info.Println(fmt.Sprintf("- program name is: %s\n", programName))
 			pterm.Info.Println(fmt.Sprintf("- program args are: %v\n", args))
 
@@ -40,7 +47,7 @@ var (
 )
 
 func init() {
-	rootCmd.Flags().IntVarP(&port, "port", "p", defaults.DefaultMultiplexerPort, "Port number for the Multiplexer server")
+	rootCmd.Flags().StringVarP(&address, "address", "a", fmt.Sprintf(":%d", defaults.DefaultMultiplexerPort), "TCP address for the MCP Proxy server (host:port)")
 }
 
 func main() {
