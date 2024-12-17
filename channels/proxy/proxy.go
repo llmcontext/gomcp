3d1b6a8ea5ebee57c235f83262cf38d4d00bac77
Pierre Carion <pcarion@gmail.com>
2024-12-17 14:34:56 -0800

    fix init sequence

--- channels/proxy/proxy.go
+++ channels/proxy/proxy.go
@@ -44,6 +44,8 @@ func (c *ProxyClient) Start() error {
 	ctx := context.Background()
 	eg, egctx := errgroup.WithContext(ctx)
 
+	// goroutine to listen for OS signals
+	// this will be used to stop the proxy client
 	eg.Go(func() error {
 		// Listen for OS signals (e.g., Ctrl+C)
 		signalChan := make(chan os.Signal, 1)
@@ -59,35 +61,40 @@ func (c *ProxyClient) Start() error {
 		}
 	})
 
+	// start the mux client
+	// create a transport for the mux client
+	muxClientSocket := socket.NewSocketClient(c.proxyInformation.MuxAddress)
+
+	// we try to start the mux client socket
+	// let's get a transport for the mux client
+	// if we fail, we return an error and stop the proxy client
+	muxClientTransport, err := muxClientSocket.Start()
+	if err != nil {
+		c.logger.Error("error starting mux client socket", types.LogArg{"error": err})
+		return err
+	}
+
+	if muxClientTransport == nil {
+		c.logger.Error("error starting mux client socket", types.LogArg{"error": err})
+		return fmt.Errorf("error creating mux transport")
+	}
+
+	// create the json rpc transport for the mux client
+	muxJsonRpcTransport := transport.NewJsonRpcTransport(muxClientTransport, "proxy client - gomcp (mux)", c.logger)
+
+	muxClient := proxymuxclient.NewProxyMuxClient(muxJsonRpcTransport, c.logger)
+
 	eg.Go(func() error {
-		// start the mux client
-		// create a transport for the mux client
-		muxClientSocket := socket.NewSocketClient(c.proxyInformation.MuxAddress)
-
-		// we try to start the mux client socket
-		// let's get a transport for the mux client
-		muxClientTransport, err := muxClientSocket.Start()
-		if err != nil {
-			c.logger.Error("error starting mux client socket", types.LogArg{"error": err})
-			return err
-		}
-
-		if muxClientTransport == nil {
-			c.logger.Error("error starting mux client socket", types.LogArg{"error": err})
-			return fmt.Errorf("error creating mux transport")
-		}
-
-		// create the json rpc transport for the mux client
-		muxJsonRpcTransport := transport.NewJsonRpcTransport(muxClientTransport, "proxy client - gomcp (mux)", c.logger)
-
-		muxClient := proxymuxclient.NewProxyMuxClient(muxJsonRpcTransport, c.logger)
-
 		err = muxClient.Start(egctx)
 		if err != nil {
 			c.logger.Error("error starting mux client", types.LogArg{"error": err})
-			return err
 		}
 
+		return err
+	})
+
+	// go routine for proxy mcp client
+	eg.Go(func() error {
 		// create the options for the proxy client
 		options := channels.ProxiedMcpServerDescription{
 			ProxyName:               GomcpProxyClientName,
@@ -112,14 +119,14 @@ func (c *ProxyClient) Start() error {
 			c.logger,
 		)
 
-		err = proxyClient.Start(egctx)
+		err := proxyClient.Start(egctx)
 		if err != nil {
 			c.logger.Error("error starting proxy client", types.LogArg{"error": err})
 		}
 		return err
 	})
 
-	err := eg.Wait()
+	err = eg.Wait()
 	if err != nil {
 		c.logger.Error("error starting proxy client", types.LogArg{"error": err})
 	}
