949b23d6ea138a813f1a4d9286f4b4daf5d240b9
Pierre Carion <pcarion@gmail.com>
2024-12-31 07:28:14 -0800

    refactor cli

---
+++ cmd/gomcp/root.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/llmcontext/gomcp/channels/hub"
+	"github.com/spf13/cobra"
+)
+
+var (
+	debug   bool
+	rootCmd = &cobra.Command{
+		Use:   "gomcp",
+		Short: "A MCP multiplexer server that enables multiple MCP proxy client connections",
+		Run: func(cmd *cobra.Command, args []string) {
+			// we create the MCP server
+			mcp, err := hub.NewHubModelContextProtocolServer(debug)
+			if err != nil {
+				fmt.Println("Error creating MCP server:", err)
+				os.Exit(1)
+			}
+
+			// start the server
+			transport := mcp.StdioTransport()
+			err = mcp.Start(transport)
+			if err != nil {
+				fmt.Println("Error starting MCP server:", err)
+				os.Exit(1)
+			}
+		},
+	}
+)
+
+func init() {
+	rootCmd.Flags().BoolVarP(&debug, "debug", "d", false, "Enable debug mode")
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+}
